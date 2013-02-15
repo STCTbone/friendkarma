e51468676ddf06a6f6e4853e19ab2ff7cd7ab5a3
Matt Rieger <matthew.rieger@gmail.com>
2013-02-15 09:58:30 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendKarma::Application.config.secret_token = '0626b8a35ac0a0efffea1cf0b42090dbf1f2e97b69ab3984de2b88ce45538d0e828a2a719e548c340f7cf9eb6f2c1d5cbeaa75a440c34a9df42d54fa26f3b2f5'
