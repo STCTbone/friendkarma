3107b4c25e4a474117e2a9a2db641905da89dbf3
Matt Rieger <matthew.rieger@gmail.com>
2013-03-13 16:20:19 -0500

    Set up Mandrill

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 FriendKarma::Application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+      :port =>           '587',
+          :address =>        'smtp.mandrillapp.com',
+              :user_name =>      ENV['MANDRILL_USERNAME'],
+                  :password =>       ENV['MANDRILL_APIKEY'],
+                      :domain =>         'heroku.com',
+                          :authentication => :plain
+}
+ActionMailer::Base.delivery_method = :smtp
