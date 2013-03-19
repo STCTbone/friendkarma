4febc698beccecf8e2b2eab4d4ac316632835840
Matt Rieger <matthew.rieger@gmail.com>
2013-03-19 10:29:18 -0500

    Got filtering working

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,13 @@ class DashboardController < ApplicationController
   def index
     @jobs = current_user.jobs
     @groups = current_user.groups
+    if params[:filter] == "Requests"
+    @activities = PublicActivity::Activity.order("created_at desc").where(trackable_type: "Request")
+    elsif params[:filter] == "Offers"
+    @activities = PublicActivity::Activity.order("created_at desc").where(trackable_type: "Job")
+    else
     @activities = PublicActivity::Activity.order("created_at desc")
+    end
   end
 
 
