f83c7b5f33e4ba681ab1f73a1e7392f8e116b587
Matt Rieger <matthew.rieger@gmail.com>
2013-03-19 22:24:34 -0500

    Fixed avatar uploading. It didn't have the right dependencies installed so make sure to bundle install and restart your server after pulling

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
@@ -29,7 +29,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-   process :scale => [165, 165]
+   process :resize_to_fit => [165, 165]
    version :thumb do
    process :resize_to_fit => [90,90 ]
    end
