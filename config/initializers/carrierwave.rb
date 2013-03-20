397b83f87ea98fb88cfe31879324ebc67e646d39
Matt Rieger <matthew.rieger@gmail.com>
2013-03-20 13:55:49 -0500

    Switched to Amazon S3 for storage of user avatars

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+	  config.fog_credentials = {
+		:provider               => 'AWS',                        # required
+		:aws_access_key_id      => '18RH5BPY7EWZ9J662TR2',                        # required
+		:aws_secret_access_key  => 'HNJAWJJlJT997N8kt2dgieiybHoM4Qhd2bXY1n3i',                        # required
+}
+		config.fog_directory  = 'FriendKarma'                     # required
+end
